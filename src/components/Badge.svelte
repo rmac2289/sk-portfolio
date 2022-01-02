59c03164884263485a71574e72757853eeaff5a4
Ross MacDonald <rossmacdonald@Rosss-Air.lan>
2022-01-02 15:08:50 -0800

    playing with background styles

---
+++ src/components/Badge.svelte
@@ -0,0 +1,109 @@
+<script>
+  import DiCodeBadge from "svelte-icons/di/DiCodeBadge.svelte";
+  import IoMdInformationCircle from "svelte-icons/io/IoMdInformationCircle.svelte";
+  import IoLogoLinkedin from "svelte-icons/io/IoLogoLinkedin.svelte";
+</script>
+
+<div class="badge">
+  <div class="hole">
+    <div class="ring">
+      <img class="string" src="ellipse.png" alt="ellipse" />
+    </div>
+  </div>
+  <div class="image">
+    <img width="180" src="headshot.png" alt="avatar" />
+  </div>
+  <div class="text">
+    <h2>Ross MacDonald</h2>
+    <div class="sub-header">
+      <h4>Full Stack Software Engineer</h4>
+    </div>
+  </div>
+  <div class="links">
+    <div class="link">
+      <a href="https://www.linkedin.com/in/rsmacdonald/">
+        <IoLogoLinkedin />
+      </a>
+    </div>
+    <div class="link">
+      <a href="/">
+        <IoMdInformationCircle />
+      </a>
+    </div>
+    <div class="link">
+      <a href="/">
+        <DiCodeBadge />
+      </a>
+    </div>
+  </div>
+</div>
+
+<style>
+  .links {
+    display: flex;
+    justify-content: space-evenly;
+    margin-top: 20px;
+    width: 50%;
+    border-radius: 15px;
+    color: white;
+  }
+  .link {
+    width: 50px;
+  }
+  a {
+    color: #000;
+  }
+  .badge {
+    width: 350px;
+    height: 550px;
+    background-image: linear-gradient(#2e4052 60%, white 50%);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    transform: rotate(-7deg);
+    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
+      0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
+      0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
+      0 100px 80px rgba(0, 0, 0, 0.12);
+    position: relative;
+  }
+
+  .badge::after {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    content: "";
+    border-bottom: 40px solid #2e4052;
+    border-bottom-left-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
+
+  .hole {
+    background: rgb(250, 239, 217);
+    height: 20px;
+    width: 60px;
+    margin-top: 15px;
+    border-radius: 5px;
+    box-shadow: inset 0 0 5px rgb(88, 78, 56);
+    position: relative;
+  }
+  .ring {
+    position: absolute;
+    top: -240px;
+    left: 20px;
+    transform: rotate(15deg);
+  }
+  .image {
+    margin-top: 30px;
+  }
+  .text {
+    color: white;
+    border-radius: 5px;
+    padding: 15px;
+    margin-top: 10px;
+    background: rgb(80, 42, 64);
+    box-shadow: -5px 5px 0px rgba(255, 200, 87), -10px 10px 0 #bdd9bf;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,6 @@
 <script>
+  import Badge from "../components/Badge.svelte";
   import { fade, fly } from "svelte/transition";
-  import DiCodeBadge from "svelte-icons/di/DiCodeBadge.svelte";
-  import IoMdInformationCircle from "svelte-icons/io/IoMdInformationCircle.svelte";
-  import IoLogoLinkedin from "svelte-icons/io/IoLogoLinkedin.svelte";
-  let showLinks = false;
-  const toggleShowLinks = () => {
-    showLinks = !showLinks;
-  };
 </script>
 
 <svelte:head>
@@ -14,39 +8,7 @@
 </svelte:head>
 <div in:fly={{ duration: 1000, y: 300, delay: 400 }} class="page-box">
   <div class="container">
-    <div class="badge">
-      <div class="hole">
-        <div class="ring">
-          <img class="string" src="ellipse.png" alt="ellipse" />
-        </div>
-      </div>
-      <div class="image">
-        <img width="180" src="headshot.png" alt="avatar" />
-      </div>
-      <div class="text">
-        <h2>Ross MacDonald</h2>
-        <div class="sub-header">
-          <h4>Full Stack Software Engineer</h4>
-        </div>
-      </div>
-      <div class="links">
-        <div class="link">
-          <a href="https://www.linkedin.com/in/rsmacdonald/">
-            <IoLogoLinkedin />
-          </a>
-        </div>
-        <div class="link">
-          <a href="/">
-            <IoMdInformationCircle />
-          </a>
-        </div>
-        <div class="link">
-          <a href="/">
-            <DiCodeBadge />
-          </a>
-        </div>
-      </div>
-    </div>
+    <Badge />
   </div>
 </div>
 
@@ -54,83 +16,19 @@
   * {
     font-family: "Roboto", sans-serif;
   }
-  .links {
-    display: flex;
-    justify-content: space-evenly;
-    margin-top: 20px;
-    width: 50%;
-    border-radius: 15px;
-    color: white;
-  }
-  .link {
-    width: 50px;
-  }
-  a {
-    color: #000;
-  }
-  .badge {
-    width: 350px;
-    height: 550px;
-    background-image: linear-gradient(#2e4052 60%, white 50%);
-    border-radius: 10px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    transform: rotate(-7deg);
-    box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-      0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-      0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-      0 100px 80px rgba(0, 0, 0, 0.12);
-    position: relative;
-  }
 
-  .badge::after {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    content: "";
-    border-bottom: 40px solid #2e4052;
-    border-bottom-left-radius: 10px;
-    border-bottom-right-radius: 10px;
-  }
-
-  .hole {
-    background: rgb(248, 226, 182);
-    height: 20px;
-    width: 60px;
-    margin-top: 15px;
-    border-radius: 5px;
-    box-shadow: inset 0 0 5px rgb(177, 155, 112);
-    position: relative;
-  }
-  .ring {
-    position: absolute;
-    top: -240px;
-    left: 0;
-  }
-  .image {
-    margin-top: 30px;
-  }
-  .text {
-    color: white;
-    border-radius: 5px;
-    padding: 15px;
-    margin-top: 10px;
-    background: rgb(80, 42, 64);
-    box-shadow: -5px 5px 0px rgba(255, 200, 87), -10px 10px 0 #bdd9bf;
-  }
   .container {
-    padding: 5%;
+    padding: 10%;
   }
+
   .page-box {
     transition: 0.75s all linear;
     height: 100vh;
     width: 100vw;
     background-image: linear-gradient(
-      45deg,
-      rgba(255, 200, 87, 0.2) 50%,
-      rgb(189, 217, 191, 0.5) 50%
+      70deg,
+      rgb(250, 239, 217) 65%,
+      rgb(189, 217, 191, 0.8) 50%
     );
   }
   @media only screen and (min-width: 520px) {
